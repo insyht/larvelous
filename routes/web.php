80b6f749bd16d05c99e9bf52d64fdaf1d2341a7a
Jordy Thijs <jthijs@rb-media.nl>
2021-10-24 21:08:48 +0200

    The routes I want to implement

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * Dashboard routes
+ */
+Route::middleware(['auth'])->prefix('dashboard')->name('dashboard')->group(function() {
+    Route::get('/', 'DashboardController@index')->name('index');
+
+    Route::get('/blocks', 'BlockController@index')->name('index');
+    Route::get('/forms', 'FormController@index')->name('index');
+    Route::get('/templates', 'TemplateController@index')->name('index');
+    Route::get('/pages', 'PageController@index')->name('index');
+    Route::get('/menus', 'MenuController@index')->name('index');
+
+    Route::get('/plugins', 'PluginController@index')->name('index');
+    Route::get('/settings', 'SettingsController@index')->name('index');
+    Route::get('/statistics', 'StatisticsController@index')->name('index');
+
+    Route::get('/media', 'MediaController@index')->name('index');
+    Route::get('/design', 'DesignController@index')->name('index');
+});
