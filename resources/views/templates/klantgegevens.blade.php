cbf7aff699521ec3f7e69d221e0d3ca9f5fa23a6
Jordy Thijs <jthijs@rb-media.nl>
2021-11-16 17:53:28 +0100

    Finished cart page

--- resources/views/templates/klantgegevens.blade.php
+++ resources/views/templates/klantgegevens.blade.php
@@ -1,4 +1,52 @@
 @extends('layouts.website')
 @section('content')
-    Klantgegevens
+    <h1>Gegevens</h1>
+
+        <div class="row mt-5">
+        <div class="col-5 col-sm-2">
+            <p>Subtotaal:</p>
+        </div>
+        <div class="col-7 col-sm-10">
+            <p>&euro; 24,75</p>
+        </div>
+        <div class="col-5 col-sm-2">
+            <p>Verzendmethode:</p>
+        </div>
+        <div class="col-7 col-sm-10">
+            <select class="form-select form-select-sm d-block d-sm-none" name="shippingMethod">
+                <option selected value="shippingMethodPostNlNoTnT">&euro;2,00 (PostNL verzending zonder track&trace)</option>
+                <option value="shippingMethodMailboxTnT">&euro;4,50 (Brievenbus pakket met track & trace)</option>
+                <option value="shippingMethodPostNlTnT">&euro;6,05 (PostNL verzending met track&trace)</option>
+            </select>
+
+            <div class="d-none d-sm-block">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="shippingMethod" id="shippingMethodPostNlNoTnT" checked value="shippingMethodPostNlNoTnT">
+                    <label class="form-check-label" for="shippingMethodPostNlNoTnT">
+                        PostNL verzending zonder track&trace: &euro;2,00
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="shippingMethod" id="shippingMethodMailboxTnT" value="shippingMethodMailboxTnT">
+                    <label class="form-check-label" for="shippingMethodMailboxTnT">
+                        Brievenbus pakket met track & trace: &euro;4,50
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="shippingMethod" id="shippingMethodPostNlTnT" value="shippingMethodPostNlTnT">
+                    <label class="form-check-label" for="shippingMethodPostNlTnT">
+                        PostNL verzending met track&trace: &euro;6,05
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="col-5 col-sm-2 mt-sm-5">
+            <p><strong>Totaal:</strong></p>
+        </div>
+        <div class="col-7 col-sm-10 mt-sm-5">
+            <p class="mb-0"><strong>&euro; 26,75</strong>
+            <p class="mt-0"><small class="text-muted">Inclusief &euro;4,64 btw</small></p>
+
+        </div>
+    </div>
 @endsection
--- resources/views/templates/winkelwagen.blade.php
+++ resources/views/templates/winkelwagen.blade.php
@@ -16,16 +16,21 @@
         <div class="col-6 col-sm-2 text-left text-sm-left mt-3">
             <p class="h4"><a href="{{ route('voorbeeld-product') }}">Frigg speen daisy maat 2 Baked clay</a></p>
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <p class="mb-0">&euro; 4,95</p> <span class="d-inline d-sm-none">&nbsp;x</span>
         </div>
-        <div class="col-4 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <input class="form-control" value="1" min="1" max="999" type="number" name="amount">
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <span class="d-inline d-sm-none">=&nbsp;</span><p class="mb-0"> &euro; 4,95</p>
         </div>
-        <div class="col-12 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 d-sm-none mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
+            <button class="btn btn-outline-primary w-100">
+                <i class="bi bi-arrow-clockwise"></i>
+            </button>
+        </div>
+        <div class="col-12 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <button class="btn btn-secondary w-100">
                 <i class="bi bi-trash"></i>
             </button>
@@ -39,16 +44,21 @@
         <div class="col-6 col-sm-2 text-left mt-3">
             <p class="h4"><a href="{{ route('voorbeeld-product') }}">Frigg speen daisy maat 2 Blush</a></p>
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <p class="mb-0">&euro; 4,95</p> <span class="d-inline d-sm-none">&nbsp;x</span>
         </div>
-        <div class="col-4 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <input class="form-control" value="3" min="1" max="999" type="number" name="amount">
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <span class="d-inline d-sm-none">=&nbsp;</span><p class="mb-0"> &euro; 14,85</p>
         </div>
-        <div class="col-12 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 d-sm-none mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
+            <button class="btn btn-outline-primary w-100">
+                <i class="bi bi-arrow-clockwise"></i>
+            </button>
+        </div>
+        <div class="col-12 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <button class="btn btn-secondary w-100">
                 <i class="bi bi-trash"></i>
             </button>
@@ -61,21 +71,46 @@
         <div class="col-6 col-sm-2 text-left text-sm-left mt-3">
             <p class="h4"><a href="{{ route('voorbeeld-product') }}">Frigg speen daisy maat 2 Cream</a></p>
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <p class="mb-0">&euro; 4,95</p> <span class="d-inline d-sm-none">&nbsp;x</span>
         </div>
-        <div class="col-4 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <input class="form-control" value="1" min="1" max="999" type="number" name="amount">
         </div>
-        <div class="col-4 col-sm-2 mt-3 d-flex align-items-center justify-content-center justify-content-sm-start">
+        <div class="col-3 col-sm-2 mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
             <span class="d-inline d-sm-none">=&nbsp;</span><p class="mb-0"> &euro; 4,95</p>
         </div>
-        <div class="col-12 col-sm-2 text-center text-sm-left mt-3 d-flex align-items-center justify-content-center">
+        <div class="col-3 d-sm-none mt-3 align-items-center align-items-sm-start d-flex justify-content-center justify-content-sm-start">
+            <button class="btn btn-outline-primary w-100">
+                <i class="bi bi-arrow-clockwise"></i>
+            </button>
+        </div>
+        <div class="col-12 col-sm-2 text-center align-items-start text-sm-left mt-3 d-flex justify-content-center">
             <button class="btn btn-secondary w-100">
                 <i class="bi bi-trash"></i>
             </button>
         </div>
     </div>
 
-{{--    Todo: kortingscode, winkelmand bijwerken, subtotaal, verzendmethode?, totaal, doorgaan--}}
+    <div class="row mt-3">
+        <div class="col-6 col-sm-2">
+            <input class="form-control w-100" type="text" name="coupon" placeholder="Kortingscode">
+        </div>
+        <div class="col-6 col-sm-2">
+            <button class="btn btn-outline-primary w-100">
+                Toepassen
+            </button>
+        </div>
+        <div class="d-none d-sm-block col-sm-2 offset-md-6 mt-3 mt-sm-0">
+            <button class="btn btn-outline-primary w-100">
+                Winkelmandje bijwerken
+            </button>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12 col-sm-2 offset-sm-10">
+            <a class="btn btn-primary w-100" href="{{ route('voorbeeld-klantgegevens') }}"><strong>Volgende stap <i class="bi bi-truck"></i></strong></a>
+        </div>
+    </div>
 @endsection
