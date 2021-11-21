e5ae37efb37a6bcfbdc752dded70023fdb91b7fe
Jordy Thijs <jthijs@rb-media.nl>
2021-11-21 20:32:04 +0100

    Bugfix: Forgot bs- prefix for Bootstrap 5.1 in 2 places

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -22,7 +22,7 @@
         <div class="row">
             <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
               <a class="navbar-brand" href="#">Musthaves4U</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
 
