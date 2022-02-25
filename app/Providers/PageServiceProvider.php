cfe8781176c38566e3836b54374c8604072e68df
Jordy Thijs <jthijs@rb-media.nl>
2022-02-25 23:09:38 +0000

    Removed todos and old comments

--- app/Providers/PageServiceProvider.php
+++ app/Providers/PageServiceProvider.php
@@ -40,15 +40,8 @@ class PageServiceProvider extends ServiceProvider
                 return;
             }
             $page = $view->offsetGet('page');
-            // todo || Ik moet nog eens goed kijken of ik de databasewaardes wel goed interpreteer. Ik zou nu 2
-            // todo || paragraph blocks moeten hebben. Eentje met een titel, text en link,
-            // todo || en eentje met titel, text, afbeelding, link en link text.. Volgens mij gebruik ik o.a.
-            // todo || block_variable_value_template_blocks.ordering verkeerd
-
-            // For all block views, save the values for this block on this view.
-            // They can be fetched from the block using $block->getBlockValues($index) in a template,
-            // where $index is the nth iteration of this block on this page
-            // todo uiteindelijk $block->addValues() voor elk $block
+            // Get the template for this page and get all blocks within that template. Then fetch all values for all
+            // blocks and save it to those blocks so that the views can show them
             foreach ($page->template->blockTemplates as $blockTemplate) {
                 /** @var \App\Block $block */
                 if ($view->getName() === $blockTemplate->block->getDottedViewPath()) {
