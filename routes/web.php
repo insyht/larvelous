ea0e3421a6c639b7dbf557fe167083cc1e05af34
Jordy Thijs <jthijs@rb-media.nl>
2023-05-05 22:20:21 +0200

    Corrected the routes

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,44 @@
 |
 */
 
+use App\Http\Controllers\Dashboard\BlockController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\Dashboard\DesignController;
+use App\Http\Controllers\Dashboard\FormController;
+use App\Http\Controllers\Dashboard\MediaController;
+use App\Http\Controllers\Dashboard\MenuController;
+use App\Http\Controllers\Dashboard\PageController;
+use App\Http\Controllers\Dashboard\PluginController;
+use App\Http\Controllers\Dashboard\SettingsController;
+use App\Http\Controllers\Dashboard\StatisticsController;
+use App\Http\Controllers\Dashboard\TemplateController;
+
 Auth::routes();
 
 /**
  * Dashboard routes
  */
+Route::middleware(['auth'])->prefix('dashboard')->name('dashboard.')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
+
+    Route::get('/blocks', [BlockController::class, 'index'])->name('blocks.index');
+    Route::get('/forms', [FormController::class, 'index'])->name('forms.index');
+    Route::get('/templates', [TemplateController::class, 'index'])->name('templates.index');
+    Route::get('/pages', [PageController::class, 'index'])->name('pages.index');
+    Route::get('/menus', [MenuController::class, 'index'])->name('menus.index');
+
+    Route::get('/media', [MediaController::class, 'index'])->name('media.index');
+    Route::get('/design', [DesignController::class, 'index'])->name('design.index');
+
+    Route::get('/plugins', [PluginController::class, 'index'])->name('plugins.index');
+    Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+    Route::get('/statistics', [StatisticsController::class, 'index'])->name('statistics.index');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::namespace('Website')->group(function () {
     Route::get('/voorbeeld/categorie', 'VoorbeeldController@categorie')->name('voorbeeld-categorie');
     Route::get('/voorbeeld/product', 'VoorbeeldController@product')->name('voorbeeld-product');
@@ -27,27 +60,3 @@ Route::namespace('Website')->group(function () {
     Route::get('/voorbeeld/contact', 'VoorbeeldController@contact')->name('voorbeeld-contact');
     Route::get('/{pageName}', 'PageController@load')->where('pageName', '.*');
 });
-
-/**
- * Dashboard routes
- */
-Route::middleware(['auth'])->prefix('dashboard')->name('dashboard.')->group(function() {
-    Route::get('/', 'DashboardController@index')->name('index');
-
-    Route::get('/blocks', 'BlockController@index')->name('blocks.index');
-    Route::get('/forms', 'FormController@index')->name('forms.index');
-    Route::get('/templates', 'TemplateController@index')->name('templates.index');
-    Route::get('/pages', 'PageController@index')->name('pages.index');
-    Route::get('/menus', 'MenuController@index')->name('menus.index');
-
-    Route::get('/media', 'MediaController@index')->name('media.index');
-    Route::get('/design', 'DesignController@index')->name('design.index');
-
-    Route::get('/plugins', 'PluginController@index')->name('plugins.index');
-    Route::get('/settings', 'SettingsController@index')->name('settings.index');
-    Route::get('/statistics', 'StatisticsController@index')->name('statistics.index');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
