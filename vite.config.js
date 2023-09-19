f656303cbf5e8559d51f648e32b1454095762a2b
Jordy Thijs <github@insyht.nl>
2023-09-19 20:15:20 +0200

    Fixed typo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            hotFile: 'public/vendor/insyht/larvelous/parvelous.hot',
+            hotFile: 'public/vendor/insyht/larvelous/larvelous.hot',
             buildDirectory: 'vendor/insyht/larvelous',
             input: [
                 'resources/sass/app.scss',
