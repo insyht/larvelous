3e93685df2174d8f76d94bcbdf503f54e1199bc6
Jordy Thijs <github@insyht.nl>
2023-10-01 23:33:31 +0200

    Load the data import of a new user after Spatie's permissions/roles migration

--- src/Providers/LarvelousServiceProvider.php
+++ src/Providers/LarvelousServiceProvider.php
@@ -18,7 +18,8 @@ class LarvelousServiceProvider extends ServiceProvider
             [
                 __DIR__ . '/../../public/images' => public_path('storage/images'),
                 __DIR__ . '/../../config/insyht-larvelous.php' => config_path('insyht-larvelous.php'),
-                __DIR__ . '/../../public/vendor/insyht/larvelous' => public_path('vendor/insyht/larvelous')
+                __DIR__ . '/../../public/vendor/insyht/larvelous' => public_path('vendor/insyht/larvelous'),
+                __DIR__ . '/../../public/vendor/insyht/larvelous/database/migrations/add_iws_admin_user.php.stub' => $this->generateMigrationName('add_iws_admin_user.php'),
             ],
             'insyht-larvelous'
         );
@@ -28,6 +29,12 @@ class LarvelousServiceProvider extends ServiceProvider
         $this->loadTranslationsFrom(__DIR__ . '/../../resources/lang', 'insyht-larvelous');
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'insyht-larvelous');
 
+        $this->publishes([
+                             __DIR__ . '/../database/migrations/create_permission_tables.php.stub' => $this->getMigrationFileName(
+                                 'create_permission_tables.php'
+                             ),
+                         ], 'permission-migrations');
+
         if ($this->app->runningInConsole()) {
             $this->commands(
                 [
@@ -35,5 +42,10 @@ class LarvelousServiceProvider extends ServiceProvider
                 ]
             );
         }
+    }
+
+    protected function generateMigrationName(string $originalName): string
+    {
+        return public_path('../database/migrations/') . substr(date('Y_m_d_His') . '_' . $originalName, 0, -4);
     }
 }
