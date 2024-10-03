3e5a1e1c8fa433a48fc3e33c8805811aff569663
Jordy Thijs <github@insyht.nl>
2024-10-03 21:34:27 +0200

    WIP 2

--- src/Forms/Components/Slide.php
+++ src/Forms/Components/Slide.php
@@ -2,10 +2,14 @@
 
 namespace Insyht\Larvelous\Forms\Components;
 
+use Closure;
 use Filament\Forms\Components\Repeater;
+use Insyht\Larvelous\Models\BlockVariableValue;
 
-class Slide extends Repeater
+class Slide extends Repeater implements BlockFieldInterface
 {
+    protected string|Closure|null $value = '';
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -18,5 +22,15 @@ class Slide extends Repeater
         $this->cloneable();
         $this->orderable();
         $this->itemLabel('Slide');
+    }
+
+    public function setExtraData(BlockVariableValue $data): static
+    {
+        return $this;
+    }
+
+    public function modify($value)
+    {
+        return $value;
     }
 }
